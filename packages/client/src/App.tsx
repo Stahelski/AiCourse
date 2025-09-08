674f4994057fb87a147c5c42444d783577b0b43a
stian <stiankarlsen709@gmail.com>
2025-09-08 19:30:14 +0200

    button shadecs

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,7 +8,12 @@ function App() {
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
-  return <p className="font-bold p-4 text-3xl">{message}</p>;
+  return (
+    <div className="p-4">
+      <p className="font-bold text-3xl">{message}</p>
+      <button className="p-3 rounded-2xl bg-gray-500">Click me</button>
+    </div>
+  );
 }
 
 export default App;
